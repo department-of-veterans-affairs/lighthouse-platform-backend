a5f1755b79a44bc54df238db9c6e6f5db3c1b05c
FonzMP <fonzmp@gmail.com>
2021-07-14 09:57:04 -0600

    user save

--- app/controllers/developer_application_controller.rb
+++ app/controllers/developer_application_controller.rb
@@ -1,7 +1,39 @@
-class DeveloperApplicationController < ApplicationController
+# frozen_string_literal: true
 
+class DeveloperApplicationController < ApplicationController
   def apply
-    puts 'Hit me!'
+    body = request_params
+    user_params = convert_request(body)
+
+    @user = User.create(user_params)
+    if @user.save
+      @user
+    else
+      puts @user.errors.full_messages
+    end
   end
 
+  private
+
+  def request_params
+    params.permit(:email,
+                  :firstName,
+                  :lastName,
+                  :organization,
+                  :description,
+                  :apis,
+                  :oAuthApplicationType,
+                  :oAuthRedirectURI,
+                  :termsOfService
+                )
+  end
+
+  def convert_request(params)
+    user_params = Hash.new
+    user_params[:email_address] = params.delete :email
+    user_params[:first_name] = params.delete :firstName
+    user_params[:last_name] = params.delete :lastName
+    user_params[:organization] = params.delete :organization
+    return user_params
+  end
 end
