cec19b7e1c6010805ea444c3cf14445f6399ea7f
FonzMP <fonzmp@gmail.com>
2021-07-14 09:45:05 -0600

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :email_address, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :organization, presence: true
 end
