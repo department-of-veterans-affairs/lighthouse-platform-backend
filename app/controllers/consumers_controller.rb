0f6e54dedd347982400caf5125271674d4c44ba5
FonzMP <fonzmp@gmail.com>
2021-07-15 09:24:14 -0600

    cleanup with association and rubocop

--- app/controllers/consumers_controller.rb
+++ app/controllers/consumers_controller.rb
@@ -4,12 +4,7 @@ class ConsumersController < ApplicationController
   def create
     @user = User.create(user_params)
     if @user.save
-      @consumer = Consumer.create(consumer_params)
-      if @consumer.save
-        puts @consumer
-      else
-        render json: { error: @consumer.errors.full_messages }, status: :ok
-      end
+      render json: { user_created: @user }, status: :ok
     else
       render json: { error: @user.errors.full_messages }, status: :ok
     end
@@ -19,27 +14,18 @@ class ConsumersController < ApplicationController
 
   def user_params
     params.require(:user).permit(
-                                  :email,
-                                  :first_name,
-                                  :last_name,
-                                  :organization,
-                                  :role
-                                )
-  end
-
-  def consumer_params
-    params.require(:consumer).permit(
-      :description,
-      :tos_accepted
+      :email,
+      :first_name,
+      :last_name,
+      :organization,
+      :role,
+      consumer_attributes: %i[
+        description
+        sandbox_gateway_ref
+        sandbox_oauth_ref
+        prod_gateway_ref
+        prod_oauth_ref
+      ]
     )
-  end
-
-  def generate_user_params(params)
-    user_params = Hash.new
-    user_params[:email_address] = params.delete :email
-    user_params[:first_name] = params.delete :firstName
-    user_params[:last_name] = params.delete :lastName
-    user_params[:organization] = params.delete :organization
-    return user_params
   end
 end
