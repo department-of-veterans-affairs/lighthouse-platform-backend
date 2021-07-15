2ca0be28d9e72ea27c2b2a7417d719c8ac996b7c
FonzMP <fonzmp@gmail.com>
2021-07-15 09:18:36 -0600

    update consumer model

--- app/models/consumer.rb
+++ app/models/consumer.rb
@@ -1,2 +1,15 @@
 class Consumer < ApplicationRecord
+  attr_accessor :tos_accepted
+
+  belongs_to :user
+
+  before_save :manage_tos
+
+  def manage_tos
+    if self.new_record? && self.tos_accepted == false
+      # raise an error
+    elsif !self.persisted?
+      self.tos_accepted_at = Time.zone.now
+    end
+  end
 end
