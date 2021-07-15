9a7ad41de6ab3a11e4faeb93c1a0215880db8a84
Charley Stran <charley.stran@gmail.com>
2021-07-15 08:50:41 -0500

    scope everything under the platform-backend route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'admin/dashboard', to: 'admin/dashboard#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # everything must scoped under platform-backend
+  scope '/platform-backend' do
+    get 'admin/dashboard', to: 'admin/dashboard#index'
+  end
 end
