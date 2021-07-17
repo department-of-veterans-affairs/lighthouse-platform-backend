22a2edafc323c5df2d3a485276cae2f2ed4f316b
Charley Stran <charley.stran@gmail.com>
2021-07-17 16:33:24 -0500

    add more verbose healtcheck

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  Healthcheck.routes(self)
   # everything must scoped under platform-backend
   scope '/platform-backend' do
     get 'admin/dashboard', to: 'admin/dashboard#index'
