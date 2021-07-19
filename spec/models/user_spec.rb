7d4c5dfd05cf73e7686685286be46a14847f3a07
FonzMP <fonzmp@gmail.com>
2021-07-19 16:11:34 -0600

    general user_spec tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,64 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:test_email) { 'chuck.norris@texas_rangers_rule.com' }
+
+  describe 'tests a valid user model' do
+
+    subject {
+      User.new(email: test_email,
+               first_name: 'Chuck',
+               last_name: 'Norris',
+               organization: 'Texas Ranger')
+    }
+
+    it 'is valid' do
+      expect(subject).to be_valid
+    end
+
+    it 'requires an email to be valid' do
+      expect(subject.email).to eq(test_email)
+    end
+
+    it 'requires a first_name to be valid' do
+      expect(subject.first_name).to eq('Chuck')
+    end
+    it 'requires a last_name to be valid' do
+      expect(subject.last_name).to eq('Norris')
+    end
+
+    it 'requires an organization to be valid' do
+      expect(subject.organization).to eq('Texas Ranger')
+    end
+  end
+
+  describe 'tests an incorrect input' do
+
+    subject {
+      User.new(email: test_email,
+        first_name: 'Chuck',
+        last_name: 'Norris',
+        organization: 'Texas Ranger')
+      }
+
+      it 'will not save without an email' do
+        subject.email = nil
+        expect(subject).to_not be_valid
+      end
+
+      it 'will not save without a first_name' do
+        subject.first_name = nil
+        expect(subject).to_not be_valid
+      end
+
+      it 'will not save without an last_name' do
+        subject.last_name = nil
+        expect(subject).to_not be_valid
+      end
+
+      it 'will not save without an organization' do
+        subject.organization = nil
+      expect(subject).to_not be_valid
+    end
+  end
 end
