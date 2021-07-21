30c5cd17e210443c7ca12bf925afd24c52185436
Charley Stran <charley.stran@gmail.com>
2021-07-21 14:59:33 -0500

    update serializer

--- app/controllers/consumers_controller.rb
+++ app/controllers/consumers_controller.rb
@@ -1,18 +1,21 @@
 # frozen_string_literal: true
 
 class ConsumersController < ApplicationController
+
   def create
+    options = {}
+    options[:include] = [:consumer]
     @user = User.find_or_initialize_by(email: params[:user][:email])
     if @user.persisted? && @user.consumer.present?
       if @user.consumer.update(user_params[:consumer_attributes])
-        render json: UserSerializer.new(@user).serializable_hash.to_json
+        render json: UserSerializer.new(@user, options).serializable_hash.to_json
       else
         render json: { error: @user.errors.full_messages }, status: :unprocessable_entity
       end
     else
       @user.assign_attributes(user_params)
       if @user.save
-        render json: UserSerializer.new(@user).serializable_hash.to_json
+        render json: UserSerializer.new(@user, options).serializable_hash.to_json
       else
         render json: { error: @user.errors.full_messages }, status: :unprocessable_entity
       end
