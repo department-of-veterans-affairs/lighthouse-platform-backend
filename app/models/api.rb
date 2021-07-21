67dd0ec88d0617045a096a6c658bbb1db99ff291
FonzMP <fonzmp@gmail.com>
2021-07-21 10:36:19 -0600

    basic api model tests

--- app/models/api.rb
+++ app/models/api.rb
@@ -3,5 +3,6 @@
 class Api < ApplicationRecord
   has_many :consumer_api_assignment, dependent: :nullify
 
+  validates_presence_of :name, :auth_method, :environment, :open_api_url, :base_path, :service_ref
   validates :api_ref, presence: true, uniqueness: { scope: :environment }
 end
