c1f93562fb7bab0cc23a5bb95c3d79c4ed047a7f
FonzMP <fonzmp@gmail.com>
2021-07-21 16:46:29 -0600

    add scope to route

--- spec/requests/consumers_request_spec.rb
+++ spec/requests/consumers_request_spec.rb
@@ -3,6 +3,7 @@
 require 'rails_helper'
 
 describe ConsumersController, type: :request do
+  base = '/platform-backend/consumers'
   let :valid_params do
     {
       user: {
@@ -31,25 +32,25 @@ describe ConsumersController, type: :request do
 
     it 'creates the user' do
       expect do
-        post '/consumers', params: valid_params
+        post base, params: valid_params
       end.to change(User, :count).by(1)
     end
 
     it 'creates the consumer' do
       expect do
-        post '/consumers', params: valid_params
+        post base, params: valid_params
       end.to change(Consumer, :count).by(1)
     end
 
     it 'builds a consumer_api_assigment to the consumer' do
       expect do
-        post '/consumers', params: valid_params
+        post base, params: valid_params
       end.to change(ConsumerApiAssignment, :count).by(2)
     end
 
     it 'responds properly when user fails to save' do
       valid_params[:user][:first_name] = nil
-      post '/consumers', params: valid_params
+      post base, params: valid_params
       rubyize_response = JSON.parse response.body
       expect(rubyize_response).to have_key('error')
       expect(rubyize_response['error'].first).to start_with('First name')
@@ -76,7 +77,7 @@ describe ConsumersController, type: :request do
     it 'updates a users APIs' do
       user = FactoryBot.create(:user, email: 'origami@oregano.com')
       consumer = FactoryBot.create(:consumer, :with_apis, user_id: user.id)
-      post '/consumers', params: update_params
+      post base, params: update_params
       expect(consumer.apis.map(&:name).sort).to eq(['Appeals API', 'Claims API', 'Forms API'])
     end
 
@@ -84,13 +85,13 @@ describe ConsumersController, type: :request do
       user = FactoryBot.create(:user, email: 'origami@oregano.com')
       consumer = FactoryBot.create(:consumer, :with_apis, user_id: user.id)
       valid_params[:user][:consumer_attributes][:apis_list] = 'va_forms'
-      post '/consumers', params: valid_params
+      post base, params: valid_params
       expect(consumer.apis.map(&:name).sort).to eq(['Claims API', 'Forms API'])
     end
 
     it 'responds properly when consumer fails to update' do
       valid_params[:user][:consumer_attributes][:description] = nil
-      post '/consumers', params: valid_params
+      post base, params: valid_params
       rubyize_response = JSON.parse response.body
       expect(rubyize_response).to have_key('error')
       expect(rubyize_response['error'].first).to start_with('Consumer description')
