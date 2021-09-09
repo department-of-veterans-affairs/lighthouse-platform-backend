0b797e503c1499c4cd69c713650e0d6511281183
Charley Stran <charley.stran@gmail.com>
2021-09-09 12:51:40 -0500

    Use figaro for Okta spec

--- app/services/okta_service.rb
+++ app/services/okta_service.rb
@@ -4,7 +4,7 @@ require 'oktakit'
 
 class OktaService
   def initialize
-    @client = Oktakit::Client.new(token: ENV['okta_token'], api_endpoint: ENV['okta_api_endpoint'])
+    @client = Oktakit::Client.new(token: Figaro.env.okta_token, api_endpoint: ENV['okta_api_endpoint'])
   end
 
   def list_applications
