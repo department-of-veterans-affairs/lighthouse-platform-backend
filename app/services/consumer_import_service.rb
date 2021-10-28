e259e49b028ae36c40058593f151d9c8419b269f
Lee DeBoom <lee.deboom@oddball.io>
2021-10-28 13:33:26 -0500

    API-9949 tests passing

--- app/services/consumer_import_service.rb
+++ app/services/consumer_import_service.rb
@@ -10,21 +10,8 @@ class ConsumerImportService
   end
 
   def import
+    update_kong_consumers
     update_dynamo_consumers
-  end
-
-  def update_dynamo_consumers
-    @dynamo_consumers.map do |dyn_consumer|
-      dyn_consumer = dyn_consumer.with_indifferent_access
-      if dyn_consumer['tosAccepted']
-        okta_application = @okta_applications.find { |okta_app| okta_app['id'] == dyn_consumer['okta_application_id'] }
-        if okta_application
-          user_model = build_user_from_dynamo(dyn_consumer, nil, okta_application['id'])
-
-          UserService.new.construct_import(user_model)
-        end
-      end
-    end
   end
 
   def build_user_from_dynamo(consumer, gateway_id, oauth_id)
@@ -44,5 +31,40 @@ class ConsumerImportService
         }
       }
     }.with_indifferent_access
+  end
+
+  def update_kong_consumers
+    @kong_consumers.map do |kong_consumer|
+      consumer = @dynamo_consumers.find { |d_consumer| kong_consumer['id'] == d_consumer['kongConsumerId'] }
+      if consumer && consumer['tosAccepted']
+        consumer = consumer.with_indifferent_access
+        update_kong_consumer(consumer, kong_consumer)
+      end
+    end
+  end
+
+  def update_kong_consumer(dynamo_consumer, consumer)
+    okta_id = dynamo_consumer['okta_application_id'] unless @okta_applications.find do |okta_app|
+      okta_app['id'] == dynamo_consumer['okta_application_id']
+    end.nil?
+
+    user_model = build_user_from_dynamo(dynamo_consumer, consumer['id'], okta_id)
+
+    UserService.new.construct_import(user_model) if dynamo_consumer['email'].present?
+    @dynamo_consumers.delete(dynamo_consumer)
+  end
+
+  def update_dynamo_consumers
+    @dynamo_consumers.map do |dyn_consumer|
+      dyn_consumer = dyn_consumer.with_indifferent_access
+      if dyn_consumer['tosAccepted']
+        okta_application = @okta_applications.find { |okta_app| okta_app['id'] == dyn_consumer['okta_application_id'] }
+        if okta_application
+          user_model = build_user_from_dynamo(dyn_consumer, nil, okta_application['id'])
+
+          UserService.new.construct_import(user_model)
+        end
+      end
+    end
   end
 end
