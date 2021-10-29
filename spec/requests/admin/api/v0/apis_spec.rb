6d27965b577e963b13da2364715d07a265cf46c8
Lee DeBoom <lee.deboom@oddball.io>
2021-10-29 15:49:03 -0500

    API-9949 up code coverage

--- spec/requests/admin/api/v0/apis_spec.rb
+++ spec/requests/admin/api/v0/apis_spec.rb
@@ -60,5 +60,19 @@ RSpec.describe 'Admin::Api::V0::Apis', type: :request do
         post '/platform-backend/admin/api/v0/apis/bulk_upload', params: { apis: valid_bulk_params }
       end.to change(Api, :count).by(1)
     end
+
+    it 'creates a new api record for bulk update endpoint via file upload' do
+      expect do
+        file = fixture_file_upload('spec/support/apis_list.csv', 'text/csv')
+        post '/platform-backend/admin/api/v0/apis/bulk_upload', params: { file: file }
+      end.to change(Api, :count).by(1)
+    end
+  end
+
+  describe 'Deleting APIs' do
+    it 'discards all apis' do
+      post '/platform-backend/admin/api/v0/apis/destroy_all'
+      expect(response.status).to eq(302)
+    end
   end
 end
