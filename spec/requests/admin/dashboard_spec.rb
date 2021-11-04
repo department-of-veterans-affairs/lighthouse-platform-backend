adf32351bda731140bb31a0dbed3a1d3f205360d
Ian McGonigle <ian.mcgonigle@oddball.io>
2021-11-04 08:03:08 -0600

    Update tests

--- spec/requests/admin/dashboard_spec.rb
+++ spec/requests/admin/dashboard_spec.rb
@@ -3,10 +3,10 @@
 require 'rails_helper'
 
 RSpec.describe 'Admin::Dashboards', type: :request do
-  describe 'GET /index' do
-    it 'returns http success' do
+  describe 'GET /platform-backend/admin/dashboard' do
+    it 'return a redirect if the user is not signed in' do
       get '/platform-backend/admin/dashboard'
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:redirect)
     end
   end
 end
