4ada95cba5d98eecaab50103dea3bdd014a09b32
FonzMP <fonzmp@gmail.com>
2021-11-29 16:27:44 -0700

    remove rubocop warning for environment model

--- app/models/environment.rb
+++ app/models/environment.rb
@@ -3,5 +3,5 @@
 class Environment < ApplicationRecord
   has_many :api_environments, dependent: :destroy
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true
 end
