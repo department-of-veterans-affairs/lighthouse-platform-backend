d047b08b3cbf9aedf34028faad6d7406dc908ebc
FonzMP <fonzmp@gmail.com>
2021-11-30 15:12:54 -0700

    add api_ref in factorybot

--- spec/factories/apis.rb
+++ spec/factories/apis.rb
@@ -4,5 +4,17 @@ FactoryBot.define do
   factory :api do
     name { 'MyString' }
     acl { 'MyString' }
+
+    trait :with_claims_api_ref do
+      after(:create) do |api, _|
+        api.api_ref = FactoryBot.create(:api_ref, name: 'claims')
+      end
+    end
+
+    trait :with_forms_api_ref do
+      after(:create) do |api, _|
+        api.api_ref = FactoryBot.create(:api_ref, name: 'va_forms')
+      end
+    end
   end
 end
--- spec/factories/consumers.rb
+++ spec/factories/consumers.rb
@@ -9,8 +9,8 @@ FactoryBot.define do
 
     trait :with_apis do
       after(:create) do |consumer, _|
-        consumer.apis << FactoryBot.create(:api, acl: 'claims', name: 'Claims API')
-        consumer.apis << FactoryBot.create(:api, acl: 'va_forms', name: 'Forms API')
+        consumer.apis << FactoryBot.create(:api, :with_claims_api_ref, acl: 'claims', name: 'Claims API')
+        consumer.apis << FactoryBot.create(:api, :with_forms_api_ref, acl: 'va_forms', name: 'Forms API')
       end
     end
   end
