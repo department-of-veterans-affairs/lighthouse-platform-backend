68cdd18b2e464fc551df16797a42724f509c1be7
FonzMP <fonzmp@gmail.com>
2022-01-10 11:40:32 -0700

    rubocop corrections

--- spec/requests/admin/dashboard_spec.rb
+++ spec/requests/admin/dashboard_spec.rb
@@ -3,21 +3,27 @@
 require 'rails_helper'
 
 RSpec.describe 'Admin::DashboardController', type: :request do
-  describe 'GET /platform-backend/admin/dashboard' do
+  describe 'GET /platform-backend/admin/dashboard with auth disabled' do
     before do
       ENV['ENABLE_GITHUB_AUTH'] = nil
     end
-    it 'returns success if auth is disabled' do
+
+    it 'returns success' do
       get '/platform-backend/admin/dashboard'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe 'GET /platform-backend/admin/dashboard' do
+  describe 'GET /platform-backend/admin/dashboard with auth enabled' do
     before do
       ENV['ENABLE_GITHUB_AUTH'] = 'true'
     end
-    it 'return a redirect if the user is not signed in' do
+
+    after do
+      ENV['ENABLE_GITHUB_AUTH'] = nil
+    end
+
+    it 'returns a redirect if the user is not signed in' do
       get '/platform-backend/admin/dashboard'
       expect(response).to have_http_status(:redirect)
     end
@@ -27,9 +33,6 @@ RSpec.describe 'Admin::DashboardController', type: :request do
       sign_in user
       get '/platform-backend/admin/dashboard'
       expect(response).to have_http_status(:success)
-    end
-    after do
-      ENV['ENABLE_GITHUB_AUTH'] = nil
     end
   end
 end
