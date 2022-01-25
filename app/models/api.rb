bb1c241be351e81a2a1d22b39f16d6a081b20624
FonzMP <fonzmp@gmail.com>
2022-01-25 13:31:12 -0700

    move API nested to reflect no change

--- app/models/api.rb
+++ app/models/api.rb
@@ -19,13 +19,13 @@ class Api < ApplicationRecord
     api_environments.undiscard_all if api_environments.present?
   end
 
-  def api_ref_attributes=(api_ref_attributes)
-    ApiRef.find_or_create_by(name: api_ref_attributes[:name], api_id: id)
-  end
-
   def api_environments_attributes=(api_environments_attributes)
     environment = Environment.find_or_create_by(name: api_environments_attributes.dig(:environments_attributes, :name))
     api_environments << ApiEnvironment.find_or_create_by(metadata_url: api_environments_attributes[:metadata_url],
                                                          environment: environment)
+  end
+
+  def api_ref_attributes=(api_ref_attributes)
+    ApiRef.find_or_create_by(name: api_ref_attributes[:name], api_id: id)
   end
 end
