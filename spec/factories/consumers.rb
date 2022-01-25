7b73aed5feb0236e6f800d537038a74dcd121708
FonzMP <fonzmp@gmail.com>
2022-01-25 13:27:00 -0700

    needed factorybot changes

--- spec/factories/consumers.rb
+++ spec/factories/consumers.rb
@@ -9,8 +9,19 @@ FactoryBot.define do
 
     trait :with_apis do
       after(:create) do |consumer, _|
-        consumer.apis << FactoryBot.create(:api, :with_claims_api_ref, acl: 'claims', name: 'Claims API')
-        consumer.apis << FactoryBot.create(:api, :with_forms_api_ref, acl: 'va_forms', name: 'Forms API')
+        environment = FactoryBot.create(:environment, name: 'test')
+
+        claims_api = FactoryBot.create(:api, :with_claims_api_ref, acl: 'claims', name: 'Claims API')
+        forms_api = FactoryBot.create(:api, :with_forms_api_ref, acl: 'va_forms', name: 'Forms API')
+
+        api_env_one = FactoryBot.create(:api_environment, api: claims_api, environment: environment)
+        api_env_two = FactoryBot.create(:api_environment, api: forms_api, environment: environment)
+        consumer.consumer_api_assignments << FactoryBot.create(:consumer_api_assignment, consumer: consumer, api_environment: api_env_one)
+        consumer.consumer_api_assignments << FactoryBot.create(:consumer_api_assignment, consumer: consumer, api_environment: api_env_two)
+
+        facilities_api = FactoryBot.create(:api, name: 'facilities', acl: 'facilities')
+        facilities_ref = FactoryBot.create(:api_ref, name: 'facilities', api_id: facilities_api.id)
+        api_env = FactoryBot.create(:api_environment, api: facilities_api, environment: environment)
       end
     end
   end
