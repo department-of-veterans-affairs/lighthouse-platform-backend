2558cf3fa300995b0f37c8386a2f77591b7ffdf9
FonzMP <fonzmp@gmail.com>
2022-02-09 11:29:46 -0700

    create mailer

---
+++ app/mailers/production_mailer.rb
@@ -0,0 +1,16 @@
+class ProductionMailer < ApplicationMailer
+  default from: Figaro.env.support_email
+
+  def consumer_production_access(request)
+    mail(to: request[:primaryContact][:email],
+         from: 'VA API Platform team',
+         subject: 'Your Request for Production Access is Submitted')
+  end
+
+  def support_production_access(request)
+    @request = request
+    mail(to: Figaro.env.support_email,
+         from: "#{request[:primaryContact][:firstName]} #{request[:primaryContact][:lastName]}",
+         subject: "Production Access Requested for #{request[:organization]}")
+  end
+end
