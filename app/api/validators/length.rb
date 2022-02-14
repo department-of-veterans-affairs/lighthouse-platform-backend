bd74f943e654d0e9b4542d0b88fc243f6d5c0060
FonzMP <fonzmp@gmail.com>
2022-02-14 13:59:15 -0700

    add length validator

---
+++ app/api/validators/length.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class Length < Grape::Validations::Validators::Base
+  def validate_param!(attr_name, params)
+    unless params[attr_name].length <= @option
+      raise Grape::Exceptions::Validation.new params: [@scope.full_name(attr_name)],
+                                              message: "must be at the most #{@option} characters long"
+    end
+  end
+end
