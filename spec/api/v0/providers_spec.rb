7d0b46b34dab343150bd5898142c420d3c7c58e9
Lee DeBoom <lee.deboom@oddball.io>
2022-02-24 09:00:09 -0600

    API-11956

--- spec/api/v0/providers_spec.rb
+++ spec/api/v0/providers_spec.rb
@@ -3,15 +3,8 @@
 require 'rails_helper'
 
 describe V0::Providers, type: :request do
-  describe 'accepts signups from dev portal' do
-    let(:api_environments) { create_list(:api_environment, 3) }
-
-    before do
-      api_environments
-      api = Api.last
-      api.update!(auth_server_access_key: 'AUTHZ_SERVER_DEFAULT')
-      api.update!(acl: nil)
-    end
+  describe 'returns list of api providers' do
+    let!(:api_environments) { create_list(:api_environment, 3) }
 
     it 'returns all apis in a form the developer-portal knows how to deal with' do
       get '/platform-backend/v0/providers/transformations/legacy'
