ce95cdd0a1372c69c77ad9ccfb195a2b6eed4159
Lee DeBoom <lee.deboom@oddball.io>
2022-03-02 14:33:06 -0600

    add specs

--- spec/api/utilities_spec.rb
+++ spec/api/utilities_spec.rb
@@ -11,27 +11,39 @@ describe Utilities, type: :request do
     end
   end
 
-  describe 'Deleting APIs' do
-    it 'discards all apis' do
+  describe 'APIs' do
+    before do
       create(:api, name: 'Claims API')
+    end
 
+    it 'gets all apis' do
+      get '/platform-backend/utilities/apis'
+      expect(response.status).to eq(200)
+    end
+
+    it 'discards all apis' do
       delete '/platform-backend/utilities/apis'
       expect(response.status).to eq(200)
     end
   end
 
-  describe 'Migrating Consumers' do
-    it 'initializes migration of consumers into new structure' do
-      post '/platform-backend/utilities/database/consumer-migration-requests'
-      expect(response.status).to eq(201)
-    end
+  it 'initializes migration of consumers into new structure' do
+    post '/platform-backend/utilities/database/consumer-migration-requests'
+    expect(response.status).to eq(201)
   end
 
-  describe 'Deleting Consumers' do
-    it 'discards all users/consumers' do
+  describe 'consumers' do
+    before do
       user = create(:user)
       create(:consumer, user_id: user.id)
+    end
 
+    it 'gets all users/consumers' do
+      get '/platform-backend/utilities/consumers'
+      expect(response.status).to eq(200)
+    end
+
+    it 'discards all users/consumers' do
       delete '/platform-backend/utilities/consumers'
       expect(response.status).to eq(200)
     end
