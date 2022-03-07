ec325b1278f06623c1d30f8446bb5a8d01eea279
Lee DeBoom <lee.deboom@oddball.io>
2022-03-07 15:48:31 -0600

    API-13868: correctly sorts apis by name

--- app/api/v0/providers.rb
+++ app/api/v0/providers.rb
@@ -27,7 +27,7 @@ module V0
       end
       get '/transformations/legacy' do
         categories = {}
-        ApiCategory.kept.joins(:api_metadatum).order('api_categories.name, api_metadata.display_name').map do |category|
+        ApiCategory.kept.map do |category|
           categories[category.key] =
             V0::Entities::Legacy::ApiProviderCategoryEntity.represent(category)
         end
