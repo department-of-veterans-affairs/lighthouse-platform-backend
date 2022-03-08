f909f9367b60956809210219130f0fbbaf881c3a
Lee DeBoom <lee.deboom@oddball.io>
2022-03-08 14:25:21 -0600

    hopefully fix tests

--- spec/factories/apis.rb
+++ spec/factories/apis.rb
@@ -2,8 +2,8 @@
 
 FactoryBot.define do
   factory :api do
-    name { Faker::Hipster.word }
-    acl { Faker::Hipster.word }
+    name { Faker::Hipster.unique.word }
+    acl { Faker::Hipster.unique.word }
     auth_server_access_key { nil }
     api_ref { association :api_ref, api: instance }
     api_metadatum { association :api_metadatum, api: instance }
