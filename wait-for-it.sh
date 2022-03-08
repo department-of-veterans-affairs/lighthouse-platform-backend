b0a9e7ee9987c1c29cfd6069748c9c43c9e46d49
FonzMP <fonzmp@gmail.com>
2022-03-08 14:00:46 -0700

    extend time, add desc

--- wait-for-it.sh
+++ wait-for-it.sh
@@ -5,15 +5,16 @@ set -e
 cmd="$@"
 attempts=0
 
+# uses elasticsearch as it tends to take the longest
 until [ "$(curl -s -o /dev/null -w '%{http_code}' 'elasticsearch:9200/_search')" -eq 200 ];
 do
-  echo "Waiting for Containers - sleeping"
+  echo "Waiting for Elasticsearch - sleeping"
   attempts=$((attempts+1))
-  if [ "$attempts" -gt 14 ]; then
+  if [ "$attempts" -gt 9 ]; then
     echo "ES failed to respond - exiting"
     exit 1
   fi
-  sleep 1
+  sleep 2
 done
 
 echo "ES is up - running tests"
