1140a60d3fabb9ecb052f1edd9f06329d176b520
Lee DeBoom <lee.deboom@oddball.io>
2022-03-15 09:25:53 -0500

    API-14244: added test for malicious url

--- spec/api/v0/consumers_spec.rb
+++ spec/api/v0/consumers_spec.rb
@@ -90,6 +90,14 @@ describe V0::Consumers, type: :request do
         expect(response.code).to eq('500')
       end
     end
+
+    context 'when url is malicious' do
+      it 'responds with bad request' do
+        signup_params[:oAuthApplicationType] = create(:malicious_url).url
+        post apply_base, params: signup_params
+        expect(response.code).to eq('400')
+      end
+    end
   end
 
   describe 'when flipper is disabled' do
