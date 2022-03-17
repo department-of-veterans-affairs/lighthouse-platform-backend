541d387a6de123a7375819787c11e027759e38d2
Alexander Wilson <fonzmp@gmail.com>
2022-03-17 10:50:53 -0600

    Correct api service (#72)
    * api_service adjustment
    
    * RUBOCOP CORRECTION?!?!

--- spec/factories/api_environments.rb
+++ spec/factories/api_environments.rb
@@ -5,5 +5,14 @@ FactoryBot.define do
     metadata_url { 'MyString' }
     api
     environment { Environment.find_or_create_by(name: 'sandbox') }
+
+    trait :auth_server_access_key do
+      after(:create) do |api_env, _|
+        api = api_env.api
+        api[:acl] = nil
+        api[:auth_server_access_key] = Faker::Hipster.word
+        api.save!
+      end
+    end
   end
 end
