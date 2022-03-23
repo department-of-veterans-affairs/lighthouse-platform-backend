ae9d7bbc73127c00a81500a2fbe5e9466e4ca525
Lee DeBoom <lee.deboom@oddball.io>
2022-03-23 13:43:37 -0500

    API-14243

--- app/api/entities/error_entity.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module Entities
-  class ErrorEntity < Grape::Entity
-    expose :title, documentation: { type: String }
-    expose :detail, documentation: { type: String }
-  end
-end
