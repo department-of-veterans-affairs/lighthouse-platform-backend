68593fb2f0d8336a3753be5d734d8d0ad0eec370
Lee DeBoom <lee.deboom@oddball.io>
2022-03-23 15:05:44 -0500

    API-14243

--- spec/models/consumer_spec.rb
+++ spec/models/consumer_spec.rb
@@ -71,8 +71,8 @@ RSpec.describe Consumer, type: :model do
   end
 
   describe 'fails on an invalid input' do
-    it 'fails without a description' do
-      subject[:description] = nil
+    it 'fails without an email' do
+      subject[:email] = nil
       expect(subject).not_to be_valid
     end
   end
