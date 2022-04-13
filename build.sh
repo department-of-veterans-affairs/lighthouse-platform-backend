9f285fe6d9901d9ffc7712cdf42e62b7be216b11
Lee DeBoom <lee.deboom@oddball.io>
2022-04-13 09:22:39 -0500

    flexible docker-compose configuration (#77)

--- build.sh
+++ build.sh
@@ -6,11 +6,11 @@ RELEASE=${RELEASE:-false}
 REPOSITORY=${ECR_REGISTRY}/lighthouse-platform-backend
 VERSION=${VERSION:-$(cat $BASEDIR/VERSION)}
 
-trap "docker-compose down -v --rmi local" EXIT
+trap "docker-compose -f docker-compose.yml -f docker-compose.dependencies.yml down -v --rmi local" EXIT
 
 echo 'Building container and running CI'
-docker-compose build --no-cache
-docker-compose run app /bin/bash wait-for-it.sh bundle exec rails db:create ci
+docker-compose -f docker-compose.yml -f docker-compose.dependencies.yml build --no-cache
+docker-compose -f docker-compose.yml -f docker-compose.dependencies.yml run app /bin/bash wait-for-it.sh bundle exec rails db:create ci
 
 echo 'Building production container...'
 docker build --pull --file $BASEDIR/Dockerfile --target prod --tag $REPOSITORY:$VERSION $BASEDIR
