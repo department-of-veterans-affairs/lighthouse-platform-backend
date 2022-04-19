f71df05ff73611e74b1694e6fe2b2b15456a8274
FonzMP <fonzmp@gmail.com>
2022-04-19 16:02:27 -0600

    auto loads sidekiq

--- app/jobs/okta_drift_job.rb
+++ app/jobs/okta_drift_job.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
-require 'sidekiq-scheduler'
+class OktaDriftJob < ApplicationJob
+  queue_as :default
 
-class OktaDriftJob
-  include Sidekiq::Worker
-
-  def perform
+  def perform(*_args)
     Okta::DriftService.new.detect_drift if Flipper.enabled?(:alert_drift)
     Okta::DriftService.new(:production).detect_drift if Flipper.enabled?(:alert_drift)
   end
