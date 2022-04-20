968a2c614da0fbf4eee60368b93a59a13f3a22ed
FonzMP <fonzmp@gmail.com>
2022-04-20 13:47:01 -0600

    slack_service_spec

---
+++ spec/services/slack_service_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe SlackService do
+  describe 'slack service' do
+    subject { SlackService.new }
+
+    it 'uses the respective webhook and message' do
+      VCR.use_cassette('slack/alert_200', match_requests_on: [:method]) do
+        result = subject.alert_slack(Figaro.env.drift_webhook, 'test')
+        expect(result.code).to eq('200')
+        expect(result).to include('server')
+        expect(result).to include('x-slack-backend')
+      end
+    end
+  end
+end
