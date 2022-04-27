3de3ee7d4fc5cf436f0788687acae43b1a008c1c
Lee DeBoom <lee.deboom@oddball.io>
2022-04-27 13:14:36 -0500

    ensure test wont randomly fail

--- spec/factories/api_categories.rb
+++ spec/factories/api_categories.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :api_category do
     name { Faker::JapaneseMedia::Naruto.village }
-    key { Faker::Lorem.word }
+    key { Faker::Lorem.unique.word }
     short_description { Faker::Lorem.sentence }
     consumer_docs_link_text { Faker::Lorem.sentence }
     veteran_redirect_link_url { Faker::Internet.url }
