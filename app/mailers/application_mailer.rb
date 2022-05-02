67a766e139491cb1812740ea43ca59f3b1d14754
Lee DeBoom <lee.deboom@oddball.io>
2022-05-02 14:15:57 -0500

    remove from

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: 'no-reply@va.gov'
+  # NOTE: Do not include default "from" here
+  #   govdelivery-tms gem handles the from and reply-to fields and will complain if non-standard values are given
+
   layout 'mailer'
 end
--- app/mailers/sandbox_mailer.rb
+++ app/mailers/sandbox_mailer.rb
@@ -8,15 +8,12 @@ class SandboxMailer < ApplicationMailer
     @kong_consumer = kong_consumer
     @okta_consumer = okta_consumer
     mail(to: request[:email],
-         from: Figaro.env.support_email,
-         reply_to: Figaro.env.support_email,
          subject: 'Welcome to the VA API Platform')
   end
 
   def va_profile_sandbox_signup(request)
     @request = request
     mail(to: Figaro.env.va_profile_distribution,
-         from: 'no-reply@va.gov',
          subject: 'VA Profile Signup Request')
   end
 end
