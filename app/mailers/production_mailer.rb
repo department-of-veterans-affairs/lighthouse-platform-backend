1d198d1bfd228d8dbf62415bc0c45c6c1179fdb2
Lee DeBoom <lee.deboom@oddball.io>
2022-05-03 14:11:22 -0500

    send display name with email (#105)

--- app/mailers/production_mailer.rb
+++ app/mailers/production_mailer.rb
@@ -3,12 +3,14 @@
 class ProductionMailer < ApplicationMailer
   def consumer_production_access(request)
     mail(to: request[:primaryContact][:email],
+         from: from_email_wrapper('VA API Platform team'),
          subject: 'Your Request for Production Access is Submitted')
   end
 
   def support_production_access(request)
     @request = request
     mail(to: Figaro.env.support_email,
+         from: from_email_wrapper("#{request[:primaryContact][:firstName]} #{request[:primaryContact][:lastName]}"),
          subject: "Production Access Requested for #{request[:organization]}")
   end
 end
