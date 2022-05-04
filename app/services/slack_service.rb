a388ca8c6773799f4a077cd2394df45d8b143a3b
FonzMP <fonzmp@gmail.com>
2022-05-04 16:14:13 -0600

    enable attachments

--- app/services/slack_service.rb
+++ app/services/slack_service.rb
@@ -6,6 +6,11 @@ class SlackService
   end
 
   def alert_slack(channel, message)
-    @client.chat_postMessage(channel: channel, text: message, as_user: true)
+    @client.chat_postMessage(channel: channel, attachments: message[:attachments], as_user: true) if use_attachments? message
+    @client.chat_postMessage(channel: channel, text: message, as_user: true) unless use_attachments? message
+  end
+
+  def use_attachments?(message)
+    message[:attachments].present?
   end
 end
