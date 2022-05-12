4306c94540003df96f095befb285c95ba3250346
FonzMP <fonzmp@gmail.com>
2022-05-12 15:29:28 -0600

    add flipper to scheduled job

--- app/jobs/weekly_signups_report_job.rb
+++ app/jobs/weekly_signups_report_job.rb
@@ -4,6 +4,6 @@ class WeeklySignupsReport < ApplicationJob
   queue_as :default
 
   def perform(*_args)
-    Slack::ReportService.new.send_weekly_report
+    Slack::ReportService.new.send_weekly_report if Flipper.enabled?(:alert_signups_report)
   end
 end
