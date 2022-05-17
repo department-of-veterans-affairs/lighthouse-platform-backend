9e1f30a16f76f88d616895401f53027c339195da
FonzMP <fonzmp@gmail.com>
2022-05-17 13:48:59 -0600

    move dynamo seed

--- spec/services/dynamo_service_spec.rb
+++ spec/services/dynamo_service_spec.rb
@@ -4,13 +4,6 @@ require 'rails_helper'
 
 RSpec.describe DynamoService do
   describe 'importing consumers from dynamo and Kong' do
-    before(:all) do
-      DynamoService.new.initialize_dynamo_db
-      DynamoService.new.seed_dynamo_db(2)
-    rescue Aws::DynamoDB::Errors::ResourceInUseException
-      # assume valid state
-    end
-
     it 'imports the users' do
       service = DynamoService.new
       results = service.fetch_dynamo_db
