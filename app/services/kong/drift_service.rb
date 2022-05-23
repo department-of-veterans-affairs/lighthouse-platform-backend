5bfe9ef1c1fd9417da6690779d736a118a2d9e58
TS000 <tylersnell@me.com>
2022-05-23 16:15:21 -0700

    time script

--- app/services/kong/drift_service.rb
+++ app/services/kong/drift_service.rb
@@ -8,7 +8,19 @@ module Kong
      end
 
     def detect_drift
-      consumers = @client.new.list_all_consumers    
+      kong_consumers = @client.new.list_all_consumers
+      time_array = [].tap do |consumer_time|
+        kong_consumers.each do |consumer|
+          consumer_time << consumer['created_at']
+        end
+      end
+      time_array.sort {|a,b| b <=> a}
+
     end
   end
 end
+
+#grab consumers from kong
+#grab consumers from lcms
+#take each consumers found and match them
+#left over consumers added to lcms
