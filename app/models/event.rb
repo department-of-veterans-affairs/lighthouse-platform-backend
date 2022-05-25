8814e25d9f44ed4adc717cef3497c8c58db3b3b2
FonzMP <fonzmp@gmail.com>
2022-05-25 15:57:08 -0600

    kidding. now it does.

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,11 +3,6 @@
 class Event < ApplicationRecord
   validates :event_type, presence: true
   validates :content, presence: true
-  scope :timeframe, ->(timeframe) { where(created_at: timeframe) }
 
   EVENT_TYPES = { sandbox_signup: 'sandbox_signup' }.freeze
-
-  def include_api?(ref)
-    content['apis'].split(',').include?(ref) if content['apis'].present?
-  end
 end
