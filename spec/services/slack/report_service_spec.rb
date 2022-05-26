d4286b13b48d731a9314aaf92c64cc9cc733ad62
FonzMP <fonzmp@gmail.com>
2022-05-26 11:45:34 -0600

    they gone.

--- spec/services/slack/report_service_spec.rb
+++ spec/services/slack/report_service_spec.rb
@@ -16,32 +16,5 @@ RSpec.describe Slack::ReportService do
         expect(result.ok).to be(true)
       end
     end
-
-    it 'calculates new and all time API signups via the events table' do
-      results = subject.send(:generate_query).first
-      api_ref = ApiRef.all.map(&:name)
-      expect(results).to include(api_ref.first).or include(api_ref.last)
-    end
-
-    it 'build the query for new signups' do
-      api_ref = ApiRef.first.name
-      results = subject.send(:new_query, api_ref)
-      expect(results).to include("content->>'apis' LIKE '%#{api_ref}%'")
-    end
-
-    it 'build the query for all time signups' do
-      api_ref = ApiRef.first.name
-      results = subject.send(:all_time_query, api_ref)
-      expect(results).to include("content->>'apis' LIKE '%#{api_ref}%'")
-    end
-
-    it 'builds the query for connection' do
-      api_refs = ApiRef.all.map(&:name)
-      results = subject.send(:build_query)
-      expect(results).to include(api_refs.first)
-      expect(results).to include(api_refs.last)
-      expect(results).to include('weekly_count')
-      expect(results).to include('total_count')
-    end
   end
 end
