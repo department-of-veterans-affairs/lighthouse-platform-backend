5278cef45e3ab5750490a6f7a206d79b4a2103e8
TS000 <tylersnell@me.com>
2022-05-27 12:24:36 -0700

    drift job

---
+++ app/jobs/kong_drift_job.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class KongDriftJob < ApplicationJob
+  queue_as :default
+
+  def perform(*_args)
+    Kong::DriftService.new.detect_drift if Flipper.enabled?(:alert_drift)
+    Kong::DriftService.new(:production).detect_drift if Flipper.enabled?(:alert_drift)
+  end
+end
