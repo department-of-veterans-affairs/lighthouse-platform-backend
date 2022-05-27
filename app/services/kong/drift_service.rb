9a005c6edd4853862244135d63991eca5af20ef0
TS000 <tylersnell@me.com>
2022-05-27 10:22:42 -0700

    fixes

--- app/services/kong/drift_service.rb
+++ app/services/kong/drift_service.rb
@@ -7,48 +7,59 @@ module Kong
     def initialize(environment = nil)
       @env = environment
       @client = production? ? Kong::ProductionService : Kong::SandboxService
+      @slack_service = Slack::AlertService.new
     end
 
     def pull_kong_consumers
       @client.new.list_all_consumers
     end
 
-    def time_sort
+    def filter_last_day
       pull_kong_consumers.filter do |consumer|
         consumer if consumer['created_at'] >= Date.yesterday.to_time.to_i
       end
     end
 
-    def detect_unknown_entities(alert_list)
-      alert_list.filter do |cid|
-        new_record? cid
+    def new_record?(consumer)
+      consumer_id = consumer['id']
+      ConsumerAuthRef.find_by(
+        '(key=? or key=?) and value=?', 
+        ConsumerAuthRef::KEYS[:sandbox_gateway_ref], 
+        ConsumerAuthRef::KEYS[:prod_gateway_ref], 
+        consumer_id
+      )
+    end
+
+    def detect_unknown_entities
+      filter_last_day.filter do |id|
+        new_record? id
       end
+    end
+
+    def detect_drift
+      consumers = @client.new.pull_kong_consumers
+      consumers_last_day = filter_last_day(consumers)
+      alert_on_list = detect_unknown_entities(consumers_last_day) unless consumers_last_day.empty?
+      alert_on_list.map { |consumer| alert_slack consumer } if alert_on_list.present?
+
+      { success: true }
     end
 
     def alert_slack(consumer)
-      @slack_service ||= SlackService.new
       message = build_message(consumer)
-      @slack_service.alert_slack(Figaro.env.slack_drift_channel, message)
+      @slack_service.send_message(Figaro.env.slack_drift_channel, { text: message })
     end
 
-    def new_record?(consumer)
-      cid = consumer['created_at']
-      if cid.present?
-        Consumer.find_by(user_id: cid)
-      end
-    end
-
-    def trim_url(url)
-      url[0...-5]
+    def production?
+      @env.eql?(:production)
     end
 
     def build_message(consumer)
-      url = trim_url(consumer[:_links][:uploadLogo][:href]) if consumer[:_links].present?
       environment = production? ? 'Production' : 'Sandbox'
       [
         '*Lighthouse Consumer Management Service Notification*',
         "Detected an unknown Consumer within Kong Environment: #{environment}",
-        "Client ID: <#{url}|#{consumer[:credentials][:oauthClient][:client_id]}>"
+        "Client ID: <#{consumer[:client_id]}>"
       ].join("\n")
     end
   end
