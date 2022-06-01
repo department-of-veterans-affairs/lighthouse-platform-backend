77dd711fd3b28ee5f6ddbd43629a253f037fc309
TS000 <tylersnell@me.com>
2022-06-01 09:42:07 -0700

    more test

---
+++ spec/jobs/kong_drift_job_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe KongDriftJob, type: :job do
+  include ActiveJob::TestHelper
+
+  subject(:job) { KongDriftJob.perform_later }
+
+  after do
+    clear_enqueued_jobs
+    clear_performed_jobs
+  end
+
+  it 'queues the job' do
+    expect { job }
+      .to change(ActiveJob::Base.queue_adapter.enqueued_jobs, :size).by(1)
+  end
+
+  it 'executes perform' do
+    allow(Kong::SandboxService).to receive(:new).and_return(true)
+    allow(Kong::ProductionService).to receive(:new).and_return(true)
+    perform_enqueued_jobs { job }
+  end
+end
