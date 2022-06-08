c0ab2387c0e483da460467a267dba62d0032e02d
FonzMP <fonzmp@gmail.com>
2022-06-08 12:55:53 -0600

    rubocop

--- spec/api/v0/consumers_spec.rb
+++ spec/api/v0/consumers_spec.rb
@@ -219,7 +219,7 @@ describe V0::Consumers, type: :request do
       get '/platform-backend/v0/consumers'
       expect(response.status).to eq(401)
     end
-    
+
     it 'receives unauthorized with an invalid token' do
       VCR.use_cassette('okta/access_token_invalid', match_requests_on: [:method]) do
         get '/platform-backend/v0/consumers', params: {}, headers: { Authorization: 'Bearer t0t4l1y-r34l' }
