f6c82a83cd05da0bb72ddd3b4393e50c8f1aaa48
FonzMP <fonzmp@gmail.com>
2022-06-16 15:22:40 -0600

    guard clause

--- app/jobs/weekly_signups_report_job.rb
+++ app/jobs/weekly_signups_report_job.rb
@@ -4,9 +4,9 @@ class WeeklySignupsReportJob < ApplicationJob
   queue_as :default
 
   def perform(*_args)
-    if Flipper.enabled?(:alert_signups_report)
-      signup_data = Slack::ReportService.new.send_weekly_report
-      Event.create(event_type: Event::EVENT_TYPES[:weekly_report], content: signup_data)
-    end
+    return unless Flipper.enabled?(:alert_signups_report)
+
+    signup_data = Slack::ReportService.new.send_weekly_report
+    Event.create(event_type: Event::EVENT_TYPES[:weekly_report], content: signup_data)
   end
 end
