3c61392391b006c78295c1bf3cbb144391625b84
FonzMP <fonzmp@gmail.com>
2022-06-22 14:40:29 -0600

    add needed event value

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,5 +4,6 @@ class Event < ApplicationRecord
   validates :event_type, presence: true
   validates :content, presence: true
 
-  EVENT_TYPES = { sandbox_signup: 'sandbox_signup', weekly_report: 'weekly_signups_report' }.freeze
+  EVENT_TYPES = { sandbox_signup: 'sandbox_signup', weekly_report: 'weekly_signups_report',
+                  monthly_report: 'monthly_signups_report' }.freeze
 end
