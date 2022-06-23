416126993b0e8d841073b1e32ee66f996b11ffb5
Alexander Wilson <fonzmp@gmail.com>
2022-06-23 11:55:58 -0600

    remove fields from entity (#161)
    * remove fields from entity
    
    * test coverage for 500 on utilities/apis
    
    * create -> build
    
    * fixes unique error

--- app/api/entities/api_environment_entity.rb
+++ app/api/entities/api_environment_entity.rb
@@ -4,9 +4,6 @@ module Entities
   class ApiEnvironmentEntity < Grape::Entity
     expose :id
     expose :metadata_url, as: :metadataUrl
-    expose :key
-    expose :label
-    expose :api_intro, as: :apiIntro
     expose :created_at, as: :createdAt
     expose :updated_at, as: :updatedAt
   end
--- spec/factories/apis.rb
+++ spec/factories/apis.rb
@@ -1,11 +1,18 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
+  sequence(:unique_api_name) do |instance|
+    "#{Faker::Lorem.word}#{instance}"
+  end
   factory :api do
-    name { Faker::Lorem.unique.word }
+    name { generate(:unique_api_name) }
     acl { Faker::Lorem.word }
     auth_server_access_key { nil }
     api_ref { association :api_ref, api: instance }
     api_metadatum { association :api_metadatum, api: instance }
+
+    trait :with_api_environment do
+      api_environments { build_list(:api_environment, 1, api: instance) }
+    end
   end
 end
