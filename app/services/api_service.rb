0d8fb2f0828e317519bf180528af343a398e7ebf
FonzMP <fonzmp@gmail.com>
2022-06-30 15:20:07 -0600

    that was easy

--- app/services/api_service.rb
+++ app/services/api_service.rb
@@ -3,15 +3,14 @@
 class ApiService
   AUTH_TYPES = %w[acg ccg apikey].freeze
 
-  def self.parse(api_list_str, filter_lpb = true)
+  def self.parse(api_list_str, filter_lpb: true)
     api_list = api_list_str.strip.split(',')
     api_list.map do |api|
       api_parts = api.strip.split('/')
       raise invalid_api_exception(api) if api_parts.blank? || api_parts.count > 2
 
       api_ref = ApiRef.kept.find_by(name: api_parts.last)
-      raise invalid_api_exception(api) if api_ref.blank?
-      raise invalid_api_exception(api) if api_ref.name.eql?('lpb') && filter_lpb
+      raise invalid_api_exception(api) if api_ref.blank? || (api_ref.name.eql?('lpb') && filter_lpb)
 
       api = api_ref.api
       api.auth_type = api_parts.count > 1 ? validate_auth_type(api_parts.first) : default_auth_type(api)
