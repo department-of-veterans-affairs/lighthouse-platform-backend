683d7f8f26732941f96a646889b362387c138f16
FonzMP <fonzmp@gmail.com>
2022-07-14 17:14:48 -0600

    update and rubocop

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,9 +30,9 @@ VCR.configure do |c|
   c.hook_into :webmock
   c.default_cassette_options = { decode_compressed_response: true }
   c.ignore_request do |request|
-    URI(request.uri).port.in?([8000, 8001, 9200]) && !URI(request.uri).path.in?(['/internal/auth/v2/validation'])
+    URI(request.uri).port.in?([4001, 4003, 4500, 8000, 8001,
+                               9200]) && !URI(request.uri).path.in?(['/internal/auth/v2/validation'])
   end
-  c.ignore_localhost = true
 end
 
 ActiveRecord::Migration.maintain_test_schema!
