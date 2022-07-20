9e4b47d38054363af2e57a6e899a06d1c5db2d8b
Lee DeBoom <lee.deboom@oddball.io>
2022-07-20 09:58:34 -0500

    tests plus the situation where sandbox_signup event has invalid content

--- spec/services/slack/report_service_spec.rb
+++ spec/services/slack/report_service_spec.rb
@@ -6,14 +6,34 @@ RSpec.describe Slack::ReportService do
   describe 'Slack report service' do
     subject { Slack::ReportService.new }
 
-    before do
-      create(:event, :sandbox_signup)
-    end
+    let!(:new_signup_event) { create(:event, :sandbox_signup) }
 
     it 'sends a valid report' do
       VCR.use_cassette('slack/report_200', match_requests_on: [:method]) do
         result = subject.send_report('week', 1.week.ago)
         expect(result.class).to be(Hash)
+      end
+    end
+
+    context 'when other event types exist' do
+      before do
+        create(:event, event_type: Event::EVENT_TYPES[:lpb_signup], created_at: 1.month.ago)
+      end
+
+      it 'queries expected values' do
+        result = subject.query_events('week', 1.week.ago)
+        expect(result['weekly_count']).to eq(1)
+      end
+    end
+
+    context 'when sandbox_signup event exists with invalid content' do
+      before do
+        create(:event, event_type: Event::EVENT_TYPES[:sandbox_signup], created_at: 1.month.ago, content: 'invalid')
+      end
+
+      it 'queries expected values' do
+        result = subject.query_events('week', 1.week.ago)
+        expect(result['weekly_count']).to eq(1)
       end
     end
   end
