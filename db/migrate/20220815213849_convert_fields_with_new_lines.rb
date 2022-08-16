998d7c312dcfd6c2c439cc58737be29981606949
Lee DeBoom <lee.deboom@oddball.io>
2022-08-16 08:48:43 -0500

    oops

--- db/migrate/20220815213849_convert_fields_with_new_lines.rb
+++ db/migrate/20220815213849_convert_fields_with_new_lines.rb
@@ -6,8 +6,8 @@ class ConvertFieldsWithNewLines < ActiveRecord::Migration[7.0]
   end
 
   def down
+    change_column :api_categories, :overview, :string
     change_column :api_categories, :quickstart, :string
-    change_column :api_categories, :quickstart, :text
-    change_column :api_release_notes, :content, :text
+    change_column :api_release_notes, :content, :string
   end
 end
