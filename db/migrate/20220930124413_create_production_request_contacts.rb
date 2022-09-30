59aee0bf36202f17d105bf39629594fa697292a6
Jeffrey Wallace <jeffrey.wallace81@gmail.com>
2022-09-30 08:29:01 -0500

    API-18379 - enhance prod request contacts schema again

--- db/migrate/20220930124413_create_production_request_contacts.rb
+++ db/migrate/20220930124413_create_production_request_contacts.rb
@@ -1,8 +1,9 @@
-class CreateSecondaryContact < ActiveRecord::Migration[7.0]
+class CreateProductionRequestContacts < ActiveRecord::Migration[7.0]
   def change
-    create_table :secondary_contacts, id: :uuid do |t|
+    create_table :production_request_contacts, id: :uuid do |t|
       t.references :production_request, null: false, foreign_key: true, type: :uuid
       t.references :user, null: false, foreign_key: true, type: :bigint
+      t.integer :contact_type
 
       t.timestamps
     end
