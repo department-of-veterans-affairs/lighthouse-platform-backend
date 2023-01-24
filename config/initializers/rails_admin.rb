17f99115d451e5a2148ed87300b9b5efccf652eb
Giles Wells <giles@mediagearhead.com>
2023-01-24 11:31:53 -0500

    Model name as a string

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -35,7 +35,7 @@ RailsAdmin.config do |config|
     # history_show
   end
 
-  config.model ApiMetadatum do
+  config.model 'ApiMetadatum' do
     show do
       configure :oauth_info do
         formatted_value do
