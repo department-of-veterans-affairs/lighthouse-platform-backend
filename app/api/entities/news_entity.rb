f22b62d6b9c3b387bf44828e3e18d48a7c61338b
Andres Rivera <andres.rivera.toro@gmail.com>
2023-02-13 11:03:28 -0500

    chore: fix indentation

--- app/api/entities/news_entity.rb
+++ app/api/entities/news_entity.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 module Entities
-    class NewsEntity < Grape::Entity
-      expose :id
-      expose :title
-      expose :url
-      expose :category
-      expose :created_at, as: :createdAt
-      expose :updated_at, as: :updatedAt
-    end
+  class NewsEntity < Grape::Entity
+    expose :id
+    expose :title
+    expose :url
+    expose :category
+    expose :created_at, as: :createdAt
+    expose :updated_at, as: :updatedAt
   end
+end
