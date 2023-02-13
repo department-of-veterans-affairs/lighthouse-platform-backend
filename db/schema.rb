78cb8ae943e438081928d94cb4e29dac26e37b2a
Andres Rivera <andres.rivera.toro@gmail.com>
2023-02-13 11:47:40 -0500

    chore: apply migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_08_190820) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_13_145053) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -186,6 +186,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_08_190820) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["url"], name: "index_malicious_urls_on_url", unique: true
+  end
+
+  create_table "news", force: :cascade do |t|
+    t.string "title"
+    t.string "url"
+    t.string "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "production_request_contacts", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
