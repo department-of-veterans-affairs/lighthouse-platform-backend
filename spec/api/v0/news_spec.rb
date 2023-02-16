574dcd8151c19f08e1727280ae3c3b03ebff354b
Andres Rivera <andres.rivera.toro@gmail.com>
2023-02-16 11:50:05 -0500

    test: add get test

---
+++ spec/api/v0/news_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe V0::News, type: :request do
+  describe 'returns list of news' do
+    it 'returns all news' do
+      get "/platform-backend/v0/news"
+      expect(response.code).to eq('200')
+    end
+  end
+end
