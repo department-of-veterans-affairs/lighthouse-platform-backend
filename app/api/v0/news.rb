0165e4a6ed90e69c33fb014e6f4579a44e112484
Andres Rivera <andres.rivera.toro@gmail.com>
2023-02-16 11:41:52 -0500

    chore: add endpoint and clean up

--- app/api/v0/news.rb
+++ app/api/v0/news.rb
@@ -13,21 +13,30 @@ module V0
         }
       }
       get '/' do
-        news = News
-        present news, with: V0::Entities::NewsEntity
+        news = NewsCategory
+        present news, with: V0::Entities::NewsCategoryEntity
       end
 
       params do
+        requires :callToAction, type: String, allow_blank: false
+        requires :description, type: String, allow_blank: false
+        requires :media, type: Boolean, allow_blank: false
+        requires :title, type: String, allow_blank: false
+      end
+      post '/category' do
+        news_category = NewsCategory.create(call_to_action: params[:callToAction], description: params[:description], media: params[:media], title: params[:title])
+        present news_category, with: V0::Entities::NewsCategoryEntity
+      end
+
+      params do
+        requires :date, type: String, allow_blank: false
+        requires :source, type: String, allow_blank: false
         requires :title, type: String, allow_blank: false
         requires :url, type: String, allow_blank: false
-        requires :category, type: String, values: %w[articles digital_media news_releases], allow_blank: false
-        given category: ->(val) { val == 'articles' || val == 'digital_media' } do
-          requires :source, type: String, allow_blank: false
-        end
       end
-      post '/' do
-        validate_token(Scope.provider_write)
-        news_item = NewsItem.create(title: params[:title], url: params[:url], category: params[:category], source: params[:source])
+      post '/item' do
+        news_category = NewsCategory.find_by!(title: params[:title])
+        news_item = NewsItem.create(news_category_id: news_category.id, date: params[:date], source: params[:source], title: params[:title], url: params[:url])
         present news_item, with: V0::Entities::NewsItemEntity
       end
     end
