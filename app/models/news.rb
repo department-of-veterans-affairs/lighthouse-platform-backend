5adc97abde58835aaf6f3dec665fcbfe1d41f1f1
Andres Rivera <andres.rivera.toro@gmail.com>
2023-02-20 16:08:13 -0500

    chore: use correct name

--- app/models/news.rb
+++ app/models/news.rb
@@ -4,5 +4,5 @@ class News < ApplicationRecord
   include Discard::Model
 
   validates :call_to_action, :description, :media, :title, presence: true
-  has_many :news_items, dependent: :destroy
+  has_many :news_item, dependent: :destroy
 end
