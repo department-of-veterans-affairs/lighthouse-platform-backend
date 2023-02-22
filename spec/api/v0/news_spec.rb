f422e7a45880cb47200a56a91ad7967d8202c48c
Andres Rivera <andres.rivera.toro@gmail.com>
2023-02-22 15:24:49 -0500

    test: add post news category

--- spec/api/v0/news_spec.rb
+++ spec/api/v0/news_spec.rb
@@ -3,16 +3,21 @@
 require 'rails_helper'
 
 describe V0::News, type: :request do
-  let(:news) { create_list(:news_category, 3) }
-
-  before do
-    news
-  end
-
   describe 'returns list of news' do
     it 'returns all news' do
       get '/platform-backend/v0/news'
       expect(response.code).to eq('200')
+    end
+  end
+
+  describe 'post news category' do
+    let(:news) { build(:news_category) }
+
+    it 'creates a valid news category' do
+      expect do
+        post '/platform-backend/v0/news/categories', params: news
+        expect(response).to have_http_status(:created)
+      end.to change(NewsCategory, :count).by 1
     end
   end
 end
