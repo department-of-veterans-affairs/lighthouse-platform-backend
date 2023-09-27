2e1394f1347029808c17eafa66c228af4a7b0f9d
Giles Wells <giles@mediagearhead.com>
2023-09-27 17:44:28 -0400

    Add rubocop bypass and shorten method

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,29 +5,20 @@ class HomeController < ApplicationController
 
   # rubocop:disable Metrics/MethodLength
   def sitemap
-    static_urls = SitemapUrl.all.pluck(:url)
-    apis = Api.includes(:api_metadatum)
-    apis.each do |api|
-      if api.api_metadatum
-        static_urls.push("/explore/api/#{api.api_metadatum[:url_slug]}")
-        if api.api_metadatum.oauth_info.present?
-          oauth = JSON.parse(api.api_metadatum[:oauth_info])
-          if oauth['acgInfo'].present?
-            static_urls.push("/explore/api/#{api.api_metadatum[:url_slug]}/authorization-code")
-          end
-          if oauth['ccgInfo'].present?
-            static_urls.push("/explore/api/#{api.api_metadatum[:url_slug]}/client-credentials")
-          end
-        end
-        static_urls.push("/explore/api/#{api.api_metadatum[:url_slug]}/release-notes")
-        unless api.api_metadatum.block_sandbox_form
-          static_urls.push("/explore/api/#{api.api_metadatum[:url_slug]}/sandbox-access")
-        end
+    urls = SitemapUrl.all.pluck(:url)
+    Api.joins(:api_metadatum).each do |api|
+      urls.push("/explore/api/#{api.api_metadatum[:url_slug]}")
+      oauth = JSON.parse(api.api_metadatum[:oauth_info]) if api.api_metadatum.oauth_info.present?
+      if oauth.present?
+        urls.push("/explore/api/#{api.api_metadatum[:url_slug]}/authorization-code") if oauth['acgInfo'].present?
+        urls.push("/explore/api/#{api.api_metadatum[:url_slug]}/client-credentials") if oauth['ccgInfo'].present?
+      end
+      urls.push("/explore/api/#{api.api_metadatum[:url_slug]}/release-notes")
+      unless api.api_metadatum.block_sandbox_form?
+        urls.push("/explore/api/#{api.api_metadatum[:url_slug]}/sandbox-access")
       end
     end
-
-    static_urls.sort
-    render :sitemap, content_type: 'text/xml', locals: { urls: static_urls }
+    render :sitemap, content_type: 'text/xml', locals: { urls: urls }
   end
   # rubocop:enable Metrics/MethodLength
 end
