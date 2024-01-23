32bcaa2790e171212cb178c76b25bb897a51f815
Giles Wells <giles@mediagearhead.com>
2024-01-23 17:25:33 -0500

    Remove the comma

--- app/services/aws_s3_service.rb
+++ app/services/aws_s3_service.rb
@@ -3,7 +3,7 @@
 class AwsS3Service
   def get_object(params)
     options = {
-      region: ENV.fetch('AWS_REGION'),
+      region: ENV.fetch('AWS_REGION')
     }
     s3 = Aws::S3::Client.new(options)
     response = s3.get_object(bucket: params[:bucket], key: params[:key])
