93d766981148ba66cabd6b71ce5b6edbdebd520f
Andrés Rivera Toro <andres.rivera.toro@gmail.com>
2024-02-05 09:45:09 -0500

    chore: replace migration file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_19_014910) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_05_143738) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
