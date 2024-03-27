4fffb8f32f25d40a97d6e80ebbea2955e0185d2b
Andrés Rivera Toro <andres.rivera.toro@gmail.com>
2024-03-27 14:48:15 -0400

    Seed news categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -2228,3 +2228,18 @@ ApiReleaseNote.create(api_metadatum_id: fhir_health_api.api_metadatum.id,
                         - Available for development environment access.
                       MARKDOWN
                      )
+NewsCategory.create(call_to_action: 'Read the articles',
+                    description: 'Articles related to developers, APIs, the developer portal, and Veterans.',
+                    media: false,
+                    title: 'Articles'
+                    )
+NewsCategory.create(call_to_action: 'Read the news releases',
+                    description: 'Press releases from the Department of Veterans Affairs related to Lighthouse.',
+                    media: false,
+                    title: 'News releases'
+                    )
+NewsCategory.create(call_to_action: 'Access digital media',
+                    description: 'Audio and video related to developers, APIs, Veterans, and the Department of Veterans Affairs.',
+                    media: true,
+                    title: 'Digital media'
+                    )
